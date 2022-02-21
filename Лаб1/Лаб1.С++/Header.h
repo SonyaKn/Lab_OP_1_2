3b2cbbbf9a09942f17b314d5d5990926ee04533b
Sonya Kondratskaya <89905308+SonyaKn@users.noreply.github.com>
2022-02-21 22:24:22 +0200

    Add files via upload

---
+++ Лаб1/Лаб1.С++/Header.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+using namespace std;
+void WriteTo(string);
+string ReadingFrom(string);
+void Func(string);
---
+++ Лаб1/Лаб1.С++/Lab1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string> 
+#include <vector>
+#include "Header.h"
+using namespace std;
+int main()
+{
+    string nameFile = "myFile.txt";
+    WriteTo(nameFile);
+    cout << endl << "Your text:" << endl<<endl;
+    string text= ReadingFrom(nameFile);
+    Func(text);
+}
